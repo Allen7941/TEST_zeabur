bed0330d01b3c6d02d99fa151a6a0e3c657b8c41
Allen <imallen0401@gmail.com>
2025-12-01 01:03:57 +0800

    調整 Flask 應用程式的執行配置，改為從環境變數讀取埠號和除錯模式

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-﻿import random
+﻿import os
+import random
 
 from flask import Flask, jsonify, render_template, request
 
@@ -54,4 +55,6 @@ def get_prizes():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("FLASK_ENV") == "development"
+    app.run(debug=debug, host="0.0.0.0", port=port)
